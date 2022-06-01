10dffa01dd8677ff091cb8320027710872d121d9
Margarita <margarita_anis@bk.ru>
2022-06-01 20:33:55 +0300

    Add project Task_33

---
+++ Task_33/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли
+// заданное число в массиве
+
+Console.Write("Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+Console.Write("Задайте число, чтобы проверить, есть ли оно в массиве: ");
+int findDigit = int.Parse(Console.ReadLine());
+
+int[] nums = new int[size];
+
+var newRnd = new Random();
+string result = $"Число {findDigit} не присутствует в массиве";
+Console.Write("Рандомный массив: [");
+for (int i = 0; i < size; i++)
+{
+    nums[i] = newRnd.Next(-10, 10);
+    Console.Write(nums[i] + " ");
+}
+Console.Write("]");
+
+Console.WriteLine();
+
+// for (int i = 0; i < size; i++)
+// {
+//     if (nums[i] == findDigit) Console.WriteLine($"Число {findDigit} присутствует в массиве");
+//     else Console.WriteLine($"Число {findDigit} не присутствует в массиве");
+//     break;
+// }
+
+for (int i = 0; i < size; i++)
+{
+    if (nums[i] == findDigit) result = $"Число {findDigit} присутствует в массиве";
+}
+Console.WriteLine(result);
