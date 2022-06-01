a34345d0cca0d34f73e5a621da1bb0514c8caba5
Margarita <margarita_anis@bk.ru>
2022-06-01 21:31:43 +0300

    Add project Task_35

---
+++ Task_35/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив из 123 элементов случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10, 99].
+
+
+int[] array = new int[123];
+int count = 0;
+
+var newRnd = new Random();
+Console.Write("Рандомный массив из 123 элементов: [");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = newRnd.Next(0, 500);
+    Console.Write(array[i] + " ");
+}
+Console.Write("]");
+
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 9 && array[i] <= 99)
+    count++;
+}
+Console.WriteLine($"Количество значений в промежутке от 10 до 99 = {count}");
+
