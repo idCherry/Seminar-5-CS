21657b647d486790373a0ed0c878be2712bb236a
Margarita <margarita_anis@bk.ru>
2022-06-01 20:02:02 +0300

    Add project Task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа замена элементов массива: положительные жлементы замените
+// на соответствующие отрицательные и наоборот.
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] numsArr = new int[size];
+
+
+var newRnd = new Random();
+Console.Write("Исходный массив: [");
+
+for (int i = 0; i < size; i++)
+{
+    numsArr[i] = newRnd.Next(-1000, 1000);
+    Console.Write(numsArr[i] + " ");
+}
+Console.Write("]");
+
+Console.WriteLine();
+
+Console.Write("Обновленный массив: [");
+for (int i = 0; i < size; i++)
+{
+    numsArr[i] *= -1;
+    Console.Write(numsArr[i] + " ");
+}
+Console.Write("]");
