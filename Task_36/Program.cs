fa2c9b864ce7f3ad2cd456a26d310f7a77e779c7
Margarita <margarita_anis@bk.ru>
2022-06-02 22:33:14 +0300

    Add HOMEWORK Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный масив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечетных позициях.
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] nums = new int[size];
+
+int sum = 0;
+
+var newRnd = new Random();
+Console.Write("Рандомный массив: [");
+for (int i = 0; i < size; i++)
+{
+    nums[i] = newRnd.Next(-100, 100);
+    Console.Write(nums[i] + " ");
+}
+Console.Write("]");
+
+Console.WriteLine();
+
+for (int i = 0; i < size; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += nums[i];
+    }
+}
+Console.WriteLine($"Сумма элементов нечетных позиций = {sum}");
